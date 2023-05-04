4c9674fa1d0d017f89fa34e7295699c330c406df
philipschulz <philip.schulz97@gmail.com>
2023-05-04 16:18:04 +0200

    image classifier test

--- hops_test/app.py
+++ hops_test/app.py
@@ -1,3 +1,11 @@
+# import the necessary packages
+import keras.utils as image_utils
+from keras.applications.imagenet_utils import decode_predictions
+from keras.applications.imagenet_utils import preprocess_input
+from keras.applications import ResNet50
+import numpy as np
+import cv2
+
 from flask import Flask
 import ghhops_server as hs
 
@@ -24,5 +32,40 @@ hops = hs.Hops(app)
 def pointat(curve: rhino3dm.Curve, t):
     return curve.PointAt(t)
 
+@hops.component(
+    "/resnet",
+    name="ResNet",
+    description="Predict image with ResNet",
+    icon="",
+    inputs=[
+        hs.HopsString("Path", "path", "Path to image")
+    ],
+    outputs=[
+        hs.HopsString("Prediction", "x", "Prediction")
+    ],
+) 
+
+def resnet(path):
+    orig = cv2.imread(path)
+    image = image_utils.load_img(path, target_size=(224, 224))
+    image = image_utils.img_to_array(image)
+
+    image = np.expand_dims(image, axis=0)
+    image = preprocess_input(image)
+
+    model = ResNet50(weights="imagenet")
+
+    preds = model.predict(image)
+    P = decode_predictions(preds)
+
+    predictions = []
+    for (i, (imagenetID, label, prob)) in enumerate(P[0]):
+        predictions.append("{}. {}: {:.2f}%".format(i + 1, label, prob * 100))
+
+    return predictions[0]
+
 if __name__ == "__main__":
     app.run()
+
+
+
